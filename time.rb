620b0d1a85a35417bc36c9cffd2989e09fd3dd0d
Tom Hird <tombjh@hotmail.co.uk>
2018-12-13 11:57:40 +0000

    done speed tests

--- time.rb
+++ time.rb
@@ -1,22 +1,11 @@
-# This part allows you to select a function you would like to test.
-puts 'Please select one of the functions to test:'
-puts '1 - Last'
-puts '2 - Reverse'
-puts '3 - Shuffle'
+require 'benchmark'
 
-choice = gets.chomp
+array = (1..1000000).map { rand }
 
-# This part gets the size of the array from the user
-
-array_size = gets.chomp.to_i
-testing_array = Array.new(array_size) {rand 1000}
-
-# now set up the timer
-
-# now start the timer
-
-# run the algorithm on the array
-
-# now stop the timer
-
-# now print the result to the user
+Benchmark.bmbm do |x|
+  x.report("sort!") { array.dup.sort! }
+  x.report("sort")  { array.dup.sort  }
+  x.report("shuffle") { array.dup.shuffle }
+  x.report("last") { array.dup.last }
+  x.report("reverse") { array.dup.last }
+end
